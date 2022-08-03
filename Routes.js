422164f911c5e151ca2bc09da8377768eb92d89d
Saloni <saloni.patil2220@gmail.com>
2022-08-03 17:26:10 +0530

    ReloadingOfDebgger

--- Routes.js
+++ Routes.js
@@ -10,6 +10,7 @@ if(url==='/'){
     res.write('<head><title>Enter Message</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
     res.write('</html>');
+    
     return res.end();
     
 }if(url==='/message' && method==='POST'){
@@ -18,11 +19,11 @@ if(url==='/'){
         console.log(chunk);
         body.push(chunk);
         
-        
+
     });
     return req.on('end',()=>{
         const parsedBody=Buffer.concat(body).toString();
-        const message=parsedBody.split('=')[1];
+        const message=parsedBody.split('=')[0];
         fs.writeFile('message.txt',message,(err)=>{
          
     res.statusCode=302;
