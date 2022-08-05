c848c919286df1232e2c30d9e84dd7b30f83682d
Saloni <saloni.patil2220@gmail.com>
2022-08-05 18:47:40 +0530

    Express1

--- RoutingResponse2.js
+++ RoutingResponse2.js
@@ -1,8 +1,20 @@
 const http=require('http');
+//core module imported
+const express=require('express');
+//third party package installed and imported
+const app=express();
+app.use((req,res,next)=>{
+    console.log('Into the middleware');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('into another middleware');
+    res.send('<h1>Hello from Express,js</h1>')
+})
 
-const routes=require('./routes.js')
 
+app.listen(3000);
 
-const server =http.createServer(routes);
+// const server =http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
