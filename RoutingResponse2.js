542da2b8910d36079089d619cfb85e0b97ad51b8
Saloni <saloni.patil2220@gmail.com>
2022-08-07 03:08:07 +0530

    RoutersAndFilters

--- RoutingResponse2.js
+++ RoutingResponse2.js
@@ -7,21 +7,19 @@ const bodyParser=require('body-parser');
 
 const app=express();
 
+const adminRoutes=require('./routes/admin.js');
+
+const shopRoutes=require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    //console.log('into another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
-});
-app.post('/product', (req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
+app.use('/admin',adminRoutes);
+
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-app.use('/',(req,res,next)=>{
-    
-    res.send('<h1>Hello from Express</h1>');
-    
-});
 
 
 app.listen(3000);
