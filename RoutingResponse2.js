19d9f115d2227ba6265e271954a106405e828663
Saloni <saloni.patil2220@gmail.com>
2022-08-07 00:45:08 +0530

    Middlewares

--- RoutingResponse2.js
+++ RoutingResponse2.js
@@ -1,16 +1,27 @@
 const http=require('http');
 //core module imported
 const express=require('express');
+
 //third party package installed and imported
+const bodyParser=require('body-parser');
+
 const app=express();
-app.use((req,res,next)=>{
-    console.log('Into the middleware');
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    //console.log('into another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
 });
-app.use((req,res,next)=>{
-    console.log('into another middleware');
-    res.send('<h1>Hello from Express,js</h1>')
+app.post('/product', (req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
+app.use('/',(req,res,next)=>{
+    
+    res.send('<h1>Hello from Express</h1>');
+    
+});
 
 
 app.listen(3000);
